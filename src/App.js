56bb996fda356fcb6e239e99cff212198296c502
Harshit Shah <harshits@kuberdigital.com>
2021-07-03 11:10:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
+import SideBar from "Components/SideBar";
+import Loader from "Components/Loader";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <Switch>
+        <Route exact path="/" component={SideBar} />
+      </Switch>
+    </Suspense>
   );
 }
 
