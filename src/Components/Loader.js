fbf373b920ed3c6084364cdfd9473ded12f5b4ad
Harshit Shah <harshits@kuberdigital.com>
2021-07-04 13:13:28 +0530

    center loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,6 +1,28 @@
 //Common Loader of the system
 import React from "react";
-import { CircularProgress } from "@material-ui/core";
-export default function Loader() {
-  return <CircularProgress />;
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  loader: {
+    margin: "auto",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: "fixed",
+    animationDuration: "550ms",
+  },
+}));
+
+function Loader() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <CircularProgress className={classes.loader} />
+    </div>
+  );
 }
+
+export default Loader;
